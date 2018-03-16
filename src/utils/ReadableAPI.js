7972fd7ef923ae195dbbe6223825d0c2eb3fe218
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-03-16 17:28:51 -0300

    feat: Add new call api
    Add call to api
     - Get all categories
     - Get all posts by category

--- src/utils/ReadableAPI.js
+++ src/utils/ReadableAPI.js
@@ -1,4 +1,4 @@
-const api = "https://localhost:3001"
+const api = "http://localhost:3001"
 
 let token = "any-token"
 
@@ -7,7 +7,15 @@ const headers = {
     'Authorization': token
 }
 
+export const getAllCategories = () =>
+    fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+export const getAllPostsByCategory = (category) =>
+    fetch(`${api}/${category}/posts`, { headers })
+    .then(res => res.json())
+
 export const getAllPosts = () =>
     fetch(`${api}/posts`, { headers })
     .then(res => res.json())
-    .then(data => data)
