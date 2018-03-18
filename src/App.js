73852ca24f8ce1442014f0f251bc676e8c3b62d1
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-03-18 20:42:48 -0300

    feat: Implement form to add new post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import Main from './components/Main'
+import PostForm from './components/PostForm'
 
 class App extends Component {
 
@@ -9,6 +10,9 @@ class App extends Component {
             <div>
                 <Route exact path='/' render={() => (
                     <Main />
+                )}/>
+                <Route exact path='/post/new' render={() => (
+                    <PostForm />
                 )}/>
             </div>
         );
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { capitalize } from '../utils/helpers'
+import AppBar from 'material-ui/AppBar'
+import { TextField, SelectField, Card, MenuItem } from 'material-ui'
+import RaisedButton from 'material-ui/RaisedButton'
 
 class PostForm extends Component {
 
     render() {
 
+        const { categories } = this.props
+        console.log(categories)
+
         return (
             <div>
-                Novo formulario
+                <AppBar
+                    title='MyLeitura'
+                    showMenuIconButton={false}
+                />
+
+                <Card style={{ padding: 20, marginLeft: 20, marginRight: 20, marginTop: 20, marginBottom: 20}}>
+                    <TextField
+                        id='title'
+                        floatingLabelText='Title'
+                    />
+                    <br />
+                    <TextField
+                        id='body'
+                        floatingLabelText='Body'
+                    />
+                    <br />
+                    <TextField
+                        id='author'
+                        floatingLabelText='Author'
+                    />
+                    <br />
+                    {categories && categories.length > 0 && (
+                        <SelectField
+                            floatingLabelText='Category'
+                        >
+                            {categories.map((category) => (
+                                <MenuItem
+                                    key={category.path}
+                                    value={category.name}
+                                    primaryText={capitalize(category.name)}
+                                />
+                            ))}
+                        </SelectField>
+                    )}
+                    <br />
+                    <RaisedButton
+                        label='Ok'
+                        primary={true}
+                    />
+                </Card>
+
             </div>
         )
     }
 }
 
-export default PostForm
+const mapStateToProps = ({ categories }) => ({
+    categories
+})
+
+export default connect(mapStateToProps, null)(PostForm);
